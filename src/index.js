5fe3d1aa2e4fa4adf372f24ae0dc973bef9755a7
Leonardo Batista <leo_batista8@hotmail.com>
2022-08-31 20:58:30 -0300

    Adding files

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+require('express-async-errors');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.use((error, request, response, next) => {
+  console.log('#### ERROR HANDLER');
+  console.log(error);
+  response.sendStatus(500);
+});
+
+app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
